405efb0a7e3ec1d20ec86d14160a68d45bf0af0e
lin-guifeng <826651687@qq.com>
2018-10-12 16:51:14 +0800

    pc

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@ define('APP_PATH', __DIR__ . '/../application/');
 require __DIR__ . '/../thinkphp/start.php';
 
 
-// 读取自动生成定义文件
-$build = include '/../build.php';
-// 运行自动生成
-\think\Build::run($build);
+// // 读取自动生成定义文件
+// $build = include '/../build.php';
+// // 运行自动生成
+// \think\Build::run($build);
---
+++ runtime/temp/cb95944ae72eba4540afa4900ed3c548.php
@@ -0,0 +1,13 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:63:"D:\lin\MyBlog\public/../application/index\view\index\index.html";i:1539334090;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<div>
+		haha
+	</div>
+</body>
+</html>
