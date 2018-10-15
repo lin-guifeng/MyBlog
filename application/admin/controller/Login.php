29e982d2ac0d94f7d8c4dbfea8d2546ea65d2348
lin-guifeng <826651687@qq.com>
2018-10-15 10:59:00 +0800

    login

---
+++ application/admin/controller/Login.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\admin\controller;
+
+class Login
+{
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function login_v2()
+    {
+        return view('login_v2');
+    }
+
+    
+}
