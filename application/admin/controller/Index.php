eda8f01b93313036badbba7138a55a4568cb3955
linguifeng <826651687@qq.com>
2019-01-17 15:31:12 +0800

    login

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -8,34 +8,12 @@ class Index extends Common
         return view('index');
     }
 
-    public function index_v1()
-    {
-        return view('index_v1');
-    }
+
     public function main()
     {
         return view('main');
     }
 
-    public function index_v2()
-    {
-        return view('index_v2');
-    }
-
-    public function index_v3()
-    {
-        return view('index_v3');
-    }
-
-    public function index_v4()
-    {
-        return view('index_v4');
-    }
-
-    public function index_v5()
-    {
-        return view('index_v5');
-    }
 //    文章列表
     public function articleList()
     {
