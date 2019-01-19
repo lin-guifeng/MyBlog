eb17a000ac5a7330224e4fd95a6d7b0584d0f23b
linguifeng <826651687@qq.com>
2019-01-19 14:23:07 +0800

    login

--- application/admin/model/Login.php
+++ application/admin/model/Login.php
@@ -14,7 +14,7 @@ class Login extends Model
 
     //记录登录信息
     public function record($data){
-        $res=db('admin')->where('password',$data['password'])->add($data);
+        $res=db('admin_record')->add($data);
         return $res;
     }
 
