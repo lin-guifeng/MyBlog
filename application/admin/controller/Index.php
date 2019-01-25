8856427cdbff84468d1d0cfa295248f03d5fd64b
linguifeng <826651687@qq.com>
2019-01-25 18:02:36 +0800

    img

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -4,27 +4,37 @@ use \think\Controller;
 use \think\Session;
 class Index extends Common
 {
-    public function index()
-    {
+    public function index(){
         $res = model('account')->recordList(session('admin_id'));
         $this->assign('res',$res);
         return view('index');
     }
 
-    public function main()
-    {
+    public function main(){
         return view('main');
     }
 
 //    文章列表
-    public function articleList()
-    {
+    public function articleList(){
         return view('article_list');
     }
 
 //    添加文章
-    public function addArticle()
-    {
+    public function addArticle(){
         return view('addArticle');
     }
+
+    public function uppic(){
+        $file = request()->file('image');
+        $info = $file->rule('md5')->move(ROOT_PATH . 'public' . DS . 'uploads');
+        if($info){
+            echo $info->getExtension();
+            echo $info->getSaveName();
+            echo $info->getFilename();
+        }else{
+            echo $file->getError();
+        }
+    }
+
+
 }
