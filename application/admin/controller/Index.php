97670865018fee6804eb935e7506d9200e973ad4
linguifeng <826651687@qq.com>
2019-01-26 10:15:34 +0800

    file

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -27,6 +27,7 @@ class Index extends Common
     public function uppic(){
         $file = request()->file('file');
         echo $file;
+        return;
         $info = $file->rule('md5')->move(ROOT_PATH . 'public' . DS . 'uploads');
         if($info){
             echo $info->getExtension();
