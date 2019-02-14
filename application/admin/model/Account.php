3aa4bde3f23cd84dc83f228ea702fc703548c095
linguifeng <826651687@qq.com>
2019-02-14 11:53:33 +0800

    group

--- application/admin/model/Account.php
+++ application/admin/model/Account.php
@@ -13,11 +13,9 @@ class Account extends Model
     public function recordList(){
         $res = db('admin_record')->alias('r')
             ->join('admin a',"a.id = r.aid")
-            ->field('r.id,r.aid,r.time,r.ip,r.area,a.name,a.user,a.group_id')
+            ->field('r.id,r.aid,r.time,r.ip,r.area,a.name,a.user,a.group_id,g.name')
+            ->join('group g',"g.id = a.group_id")
             ->paginate(10);
-        foreach ($res as &$val){
-            $val['group'] = db('admin_group')->where('id',$val['group_id'])->field('name');
-        }
         return $res;
     }
 
