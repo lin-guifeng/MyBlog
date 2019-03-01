f9d6251b9ab2c235b3908d57495046925bb131cc
linguifeng <826651687@qq.com>
2019-03-01 17:56:10 +0800

    lunbo

--- application/admin/controller/Picture.php
+++ application/admin/controller/Picture.php
@@ -21,7 +21,7 @@ class Picture extends Common
     public function lunboAdd(){
         if ($this->request->isPost()){
             $ajaxData = $this->request->post();
-            return json_encode($ajaxData);
+
             $data['url'] = $ajaxData['url'];
             $data['name'] = $ajaxData['name'];
             $data['sort'] = $ajaxData['sort'];
@@ -41,6 +41,7 @@ class Picture extends Common
         $res = model('picture')->lunboFind($id);
         if ($this->request->isPost()){
             $ajaxData = $this->request->post();
+            return json_encode($ajaxData);
             $data['url'] = $ajaxData['url'];
             $data['name'] = $ajaxData['name'];
             $data['sort'] = $ajaxData['sort'];
