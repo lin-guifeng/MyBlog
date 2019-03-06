d6e079e0dd554a92ad12bf360fd6a12b9c1b80ee
linguifeng <826651687@qq.com>
2019-03-06 18:27:58 +0800

    common

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -18,6 +18,15 @@ class Common extends Controller
             session('long_time',time());
         }
     }
+    public function getHtml(){
+        //搜索指定关键词的百度图片并显示
+        $keyword = "美女";
+        $keyword = urlencode($keyword);
+        $url = "https://image.baidu.com/search/index?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&fm=index&fr=&hs=0&xthttps=111111&sf=1&fmq=&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&ie=utf-8&word=%E7%BE%8E%E5%A5%B3&oq=meinv&rsp=0";
+        $html = file_get_contents($url);
+        preg_match_all("/\"[^\"]*[^0]\.jpg\"/", $html, $text);
+        dump($text);
+    }
 
 }
 ?>
