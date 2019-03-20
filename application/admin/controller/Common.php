8638b6320111ce4db370a2b25a7ace86a560a442
linguifeng <826651687@qq.com>
2019-03-20 12:00:16 +0800

    asd

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -64,6 +64,18 @@ class Common extends Controller
         $html = json_decode($html,true);
         dump($html);
     }
+    public function getall(){
+        $keyword = "美女";
+        $keyword = urlencode($keyword);
+        $pn = 30;
+        $gsm = binhex($pn);
+        $url = "https://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&is=&fp=result&queryWord=".$keyword."&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=&hd=&latest=&copyright=&word=".$keyword."&s=&se=&tab=&width=&height=&face=0&istype=2&qc=&nc=1&fr=&expermode=&force=&pn=".$pn."&rn=30&gsm=".$gsm;
+
+
+        $html = file_get_contents($url);
+        $html = json_decode($html,true);
+        dump($html);
+    }
 
 }
 ?>
