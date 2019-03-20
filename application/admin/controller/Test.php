7af85ffc9e37be35d9f007258b9106b4dbbb4a04
linguifeng <826651687@qq.com>
2019-03-20 09:24:09 +0800

    爬虫

--- application/admin/controller/Test.php
+++ application/admin/controller/Test.php
@@ -1,7 +1,22 @@
 <?php
 namespace app\admin\controller;
 
-class Test
+class Test extends Common
 {
+    public function paqu(){
 
+        //搜索指定关键词的百度图片并显示
+        $keyword = "风景";
+        $keyword = urlencode($keyword);
+        $url = "http://image.baidu.com/search/index?tn=baiduimage&word=" . $keyword;
+        $html = file_get_contents($url);
+        preg_match_all("/\"[^\"]*[^0]\.jpg\"/", $html, $text);
+        foreach ($text as $key => $value) {
+            foreach ($value as $img) {
+                print "<img src=" . $img . " style='width:30px;'/>";
+            }
+        }
+
+
+    }
 }
