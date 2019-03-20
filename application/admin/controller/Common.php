756f3b4cc750689d348a1164aee221f652215202
linguifeng <826651687@qq.com>
2019-03-20 18:29:26 +0800

    asd

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -92,7 +92,7 @@ class Common extends Controller
             $url = "https://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&is=&fp=result&queryWord=".$keywords."&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=&hd=&latest=&copyright=&word=".$keywords."&s=&se=&tab=&width=&height=&face=0&istype=2&qc=&nc=1&fr=&expermode=&force=&pn=".$pn."&rn=30&gsm=".$gsm;
             $con = file_get_contents($url);
             $con = json_decode($con,true);
-            $res = empty($con['data']);
+            $res = empty($con['data']['0']);
             return $res;
             exit;
 //            $html = array_merge((array)$html,(array)$con['data']);
