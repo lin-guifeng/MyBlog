a7e1ea5cd7958c8bbc1d69201c0536aec6f8716f
linguifeng <826651687@qq.com>
2019-03-20 10:20:45 +0800

    asd

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -61,6 +61,7 @@ class Common extends Controller
     public function get4(){
         $url = "https://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&is=&fp=result&queryWord=%E9%A3%8E%E6%99%AF&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=&hd=&latest=&copyright=&word=%E9%A3%8E%E6%99%AF&s=&se=&tab=&width=&height=&face=0&istype=2&qc=&nc=1&fr=&expermode=&force=&pn=120&rn=30&gsm=78&1553046735861=";
         $html = file_get_contents($url);
+        $html = json_decode($html,true);
         dump($html);
     }
 
