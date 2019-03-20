9647697e11c36bd3bc7f8ee11b4e2cbca1764e97
linguifeng <826651687@qq.com>
2019-03-20 09:39:35 +0800

    爬虫

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -27,6 +27,22 @@ class Common extends Controller
         preg_match_all("/\"[^\"]*[^0]\.jpg\"/", $html, $text);
         dump($text);
     }
+    public function paqu(){
+        //搜索指定关键词的百度图片并显示
+        $keyword = "风景";
+        $keyword = urlencode($keyword);
+        $url = "http://image.baidu.com/search/index?tn=baiduimage&word=" . $keyword;
+        $html = file_get_contents($url);
+        preg_match_all("/\"[^\"]*[^0]\.jpg\"/", $html, $text);
+        dump($text);
+//        foreach ($text as $key => $value) {
+//            foreach ($value as $img) {
+//                print "<img src=" . $img . " style='width:30px;'/>";
+//            }
+//        }
+
+
+    }
 
 }
 ?>
