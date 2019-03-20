5048029f8e690705aa7bbd6e6be8761200423600
linguifeng <826651687@qq.com>
2019-03-20 18:51:04 +0800

    asd

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -85,19 +85,21 @@ class Common extends Controller
 
         $html = [];
         $t1=microtime(true);
-        for ($i=0;$i<$num;$i++){
+
+//        for ($i=0;$i<$num;$i++){
+        $i=100;
             $pn = 30+30*$i;
             $gsm = base_convert($pn, 10, 16);
             $url = "https://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&is=&fp=result&queryWord=".$keywords."&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=&hd=&latest=&copyright=&word=".$keywords."&s=&se=&tab=&width=&height=&face=0&istype=2&qc=&nc=1&fr=&expermode=&force=&pn=".$pn."&rn=30&gsm=".$gsm;
             $con = file_get_contents($url);
             $con = json_decode($con,true);
 
-            $html = array_merge((array)$html,(array)$con['data']);
-            if(empty($con['data'])==true){
-                break;
-            }
-        }
-        return $html;
+//            $html = array_merge((array)$html,(array)$con['data']);
+//            if(empty($con['data'])==true){
+//                break;
+//            }
+//        }
+        return $con;
         exit;
         $t2=microtime(true);
         foreach($html as $key=>$value){
