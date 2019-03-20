581a7ad3465d743e1fd012f58e7465989c9ff8d3
linguifeng <826651687@qq.com>
2019-03-20 13:30:53 +0800

    asd

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -70,6 +70,7 @@ class Common extends Controller
         $pn = 30;
         $gsm = base_convert($pn, 10, 16);
         echo $gsm;
+        echo dechex(30);
         $url = "https://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&is=&fp=result&queryWord=".$keyword."&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=&hd=&latest=&copyright=&word=".$keyword."&s=&se=&tab=&width=&height=&face=0&istype=2&qc=&nc=1&fr=&expermode=&force=&pn=".$pn."&rn=30&gsm=".$gsm;
 
 
