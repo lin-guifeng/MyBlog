053a2419dd09112b87a54193f3ed7a3dbd330efe
linguifeng <826651687@qq.com>
2019-03-28 10:20:46 +0800

    tuwan

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -98,7 +98,7 @@ class Common extends Controller
             $urls = "https://api.tuwan.com/apps/Welfare/detail?type=image&dpr=3&id=".$value['id']."&callback=jQuery112301655331505750104_1553649347144&_=1553649347145";
             $cons = file_get_contents($urls);
 //            $cons = json_decode($cons,true);
-            $value['album'] = $cons;
+//            $value['album'] = $cons;
 //            $htmls = array_merge((array)$html,(array)$cons['data']);
         }
         return $html;
