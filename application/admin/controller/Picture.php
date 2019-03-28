072fdbddd5f1356edc57a463c5c38d6c26f4fb96
linguifeng <826651687@qq.com>
2019-03-28 11:12:50 +0800

    tuwan

--- application/admin/controller/Picture.php
+++ application/admin/controller/Picture.php
@@ -153,20 +153,15 @@ class Picture extends Common
     }
     //    批量导入兔玩网图片
     public function tuwanAdd(){
-
-
-            $data = $this->getTuwan(input('page'),input('callnum'));
-            $res = model('picture')->tuwanAdd($data);
-
-
-
-
-        if($res){
-            $this->success("批量添加图片成功","/admin/picture/tuwanList");
-        }else{
-            $this->error("批量添加图片失败","/admin/picture/tuwanList");
-        }
-
+        $data = $this->getTuwan(input('page'),input('callnum'));
+        $res = model('picture')->tuwanAdd($data);
+        echo $res;
+        return;
+//        if($res){
+//            $this->success("批量添加图片成功","/admin/picture/tuwanList");
+//        }else{
+//            $this->error("批量添加图片失败","/admin/picture/tuwanList");
+//        }
     }
     //    兔玩网图片删除
     public function tuwanDel(){
