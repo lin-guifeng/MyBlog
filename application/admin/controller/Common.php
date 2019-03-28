6341e48c22680b8faff06b16e249cd748fa1877f
linguifeng <826651687@qq.com>
2019-03-28 09:22:38 +0800

    tuwan

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -74,13 +74,13 @@ class Common extends Controller
     }
 
     public function getTuwan(){
-        $html = [];
+//        $html = [];
         $htmls = [];
         $callnum = '1553681240966';
         $i=1;
         $url = "https://api.tuwan.com/apps/Welfare/getMenuList?from=pc&format=jsonp&page=".$i."&callback=jQuery1123009817294954161926_1553681240965&_=".$callnum;
-        $html = file_get_contents($url);
-//        $html = json_decode($con,true);
+        $con = file_get_contents($url);
+        $html = json_decode($con,true);
 //        $html = array_merge((array)$html,(array)$con['data']);
 //        for ($i=1;$i<=32;$i++){
 //
