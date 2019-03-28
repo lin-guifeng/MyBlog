52236c8594ee647595a84969e8ec947a5442fbd9
linguifeng <826651687@qq.com>
2019-03-28 14:02:42 +0800

    tuwan

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -84,6 +84,8 @@ class Common extends Controller
         foreach($html['data'] as $key=>$value){
             $urls = "https://api.tuwan.com/apps/Welfare/detail?type=image&dpr=3&id=".$value['id']."&callback=jQuery112301655331505750104_1553649347144&_=1553649347147";
             $cons = file_get_contents($urls);
+            $con = substr($con,strpos($con,'(')+1);
+            $con = substr($con, 0, -1);
 //            $cons = substr($cons, strlen('(')+strpos($cons, '('),(strlen($cons) - strpos($cons, ')'))*(-1));
 //            $cons = json_decode($cons,true);
             $res[$key]['abc'] = $cons;
