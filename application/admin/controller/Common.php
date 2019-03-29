249a47aa63b941ebf64b531656d664796eb1fbc6
linguifeng <826651687@qq.com>
2019-03-29 15:42:03 +0800

    tuwan

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -82,7 +82,7 @@ class Common extends Controller
         $res = [];
         $html = [];
 //        foreach($html['data'] as $key=>$value){
-        for($i=1;$i<=600;$i++){
+        for($i=1;$i<=100;$i++){
             $key = urlencode($i);
 //            $urls = "https://api.tuwan.com/apps/Welfare/detail?type=image&dpr=3&id=".$value['id']."&callback=jQuery112301655331505750104_1553649347144&_=1553649347147";
             $urls = "https://api.tuwan.com/apps/Welfare/detail?type=image&dpr=3&id=".$key."&callback=jQuery112301655331505750104_1553649347144&_=1553649347147";
@@ -93,9 +93,10 @@ class Common extends Controller
             $cons = substr($cons, 0, -1);
             $cons = json_decode($cons,true);
             if($cons['error']=='0'){
-//                $res['pic'] = $cons['pic'];
+                if (isset($cons['pic'])){$res['pic'] = $cons['pic'];}
+                $res['thumb'] = json_encode($cons['thumb']);
                 $res['id'] = $cons['id'];
-                $html = array_merge((array)$html,(array)$cons['id']);
+                $html = array_merge((array)$html,(array)$res);
 
 //                $html['data'] = json_encode($cons['data']);
 
