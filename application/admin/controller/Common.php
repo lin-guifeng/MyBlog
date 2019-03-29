af79b648411719825bd9a3bb838c0bf4b119658a
linguifeng <826651687@qq.com>
2019-03-29 14:02:10 +0800

    tuwan

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -87,7 +87,8 @@ class Common extends Controller
 //            $urls = "https://api.tuwan.com/apps/Welfare/detail?type=image&dpr=3&id=".$value['id']."&callback=jQuery112301655331505750104_1553649347144&_=1553649347147";
             $urls = "https://api.tuwan.com/apps/Welfare/detail?type=image&dpr=3&id=".$key."&callback=jQuery112301655331505750104_1553649347144&_=1553649347147";
             $urls = preg_replace("/ /", "%20", $urls);
-            $cons = file_get_contents($urls);
+//            $cons = file_get_contents($urls);
+            $cons = get( $urls, $_header = NULL );
             $cons = substr($cons,strpos($cons,'(')+1);
             $cons = substr($cons, 0, -1);
             $cons = json_decode($cons,true);
