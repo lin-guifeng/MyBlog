c8b959328fccb8698f9cf0f5db91f4d3ed291faf
linguifeng <826651687@qq.com>
2019-04-03 11:14:46 +0800

    tuwan

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -46,48 +46,25 @@ class Index extends Common
 
 //    测试页面
     public function test(){
-//        $arr = [
-//            '腿控','女仆','标准福利','兔女郎','旗袍','动漫类','JK制服','运动体操服','黑丝','御姐','网袜','肉丝','其他服装','萝莉','白丝','比基尼','巨乳','死库水','大福利','保守','游戏类','脚控','吊带袜','轻剧情','洛丽塔','丰满微胖'
-//        ];
-//        13位时间戳
-//        list($t1, $t2) = explode(' ', microtime());
-//        $time = (float)sprintf('%.0f',(floatval($t1)+floatval($t2))*1000);
-//        echo "时间戳：".$time;
-//        dump($arr);
-//        foreach ($arr as $val){
-//            $res = db('tuwan_tags')->insert(['name'=>$val]);
-//        }
+
         if ($this->request->isPost()){
             $page = $this->request->post('page');
-            $num = 100;
+            $num = 10;
             $start = ($page-1)*$num;
-            $data=db('tuwan_url')->limit($start,$num)->select();
+            $data=db('tuwan')->limit($start,$num)->select();
+            $res = [];
             foreach ($data as $key => $val){
-                $val['text'] = substr($val['text'],strpos($val['text'],'(')+1);
-                $val['text'] = substr($val['text'], 0, -1);
-                $val['text'] = json_decode($val['text'],true);
-                if($val['text']!=null){
-                    if($val['text']['error']!='1'&&$val['text']['thumb']!=null){
-//                        $res[$key] = $val['text'];
-//                        $res[$key]['pic'] = json_encode($val['text']['pic']);
-                        $res[$key]['tags'] = json_encode($val['text']['tags']);
-                        $res[$key]['thumb'] = json_encode($val['text']['thumb']);
-                        $res[$key]['title'] = $val['text']['title'];
-                        $res[$key]['bgm'] = $val['text']['bgm'];
-                        $res[$key]['bgm_name'] = $val['text']['bgm_name'];
-                        $res[$key]['bgm_img'] = $val['text']['bgm_img'];
-                        $res[$key]['pid'] = $val['text']['id'];
-                        $res[$key]['data'] = json_encode($val['text']['data']);
-                    }
-                }
+                $res[$key]['data'] = json_decode($val['data']);
+                $res[$key]['thumb'] = json_decode($val['thumb']);
+                $res[$key]['id'] = $val['id'];
             }
-            $tuwan=db('tuwan')->insertAll($res);
-//            return $res;
-            if($tuwan){
-                return ['data'=>$tuwan,'code'=>1,'message'=>'操作完成'];
-            }else{
-                return ['data'=>$tuwan,'code'=>0,'message'=>'操作失败'];
-            }
+//            $tuwan=db('tuwan')->saveAll($res);
+            return $res;
+//            if($tuwan){
+//                return ['data'=>$tuwan,'code'=>1,'message'=>'操作完成'];
+//            }else{
+//                return ['data'=>$tuwan,'code'=>0,'message'=>'操作失败'];
+//            }
         }
 
         return view('test');
