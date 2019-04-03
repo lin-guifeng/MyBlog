6586dc561088e08938c7a2688096aeea4faac80f
linguifeng <826651687@qq.com>
2019-04-03 13:52:03 +0800

    tuwan

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -62,6 +62,9 @@ class Index extends Common
                 $data_pic = json_decode($val['data'])['0'];
 //                $data_pic = json_decode($data_pic);
                 for ($i=3;$i<count($res[$key]['thumb']);$i++){
+                    foreach ($data_pic as $k => $v){
+                        $data_pic[$k] = 123;
+                    }
                     array_push($res[$key]['data'],$data_pic);
                 }
 
